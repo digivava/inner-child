68299e4baa168d011f340736839e7c4dd5670dc9
Valerie <val.conklin@gmail.com>
2016-06-02 08:26:05 -0600

    Still no internet... need to find a cafe here in Salt Lake City where I can do my homework. But there's the skeleton of a test.

--- test/models/candy_test.rb
+++ test/models/candy_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
+# make sure test_helper has VCR
 
 class CandyTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # once I get internet access I need to set this up so it actually connects with VCR
+
+  describe "API" do
+    before do
+      # find a candy through Walmart API
+    end
+
+    it "should return JSON", :vcr do
+      # ____.is_a? JSON
+    end
+
+  end
 end
