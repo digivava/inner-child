658814957e7c3b19570c03d1017827e1c84c5e0b
Valerie <val.conklin@gmail.com>
2016-06-02 23:13:43 -0600

    Added beginning to class method for searching the walmart API using httparty

--- app/models/candy.rb
+++ app/models/candy.rb
@@ -1,2 +1,12 @@
+require 'httparty'
+
 class Candy < ActiveRecord::Base
+
+  BASE_URL = "http://api.walmartlabs.com/v1/"
+
+  def self.search(query)
+    candy = HTTParty.get(BASE_URL + "search?apiKey=#{ENV['WALMART_KEY']}&query=#{query}")
+    binding.pry
+  end
+
 end
