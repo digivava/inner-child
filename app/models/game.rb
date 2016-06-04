0365c6c11ac9b4ebc051f27fdd2d8b6d56fdf622
Valerie <val.conklin@gmail.com>
2016-06-03 22:24:22 -0600

    Game search now responds to actual query, and its respective box art image is now shown in the view

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,17 +1,17 @@
 class Game
 
   BASE_URL = "http://www.giantbomb.com/api/"
-  attr_reader :id, :name, :rating
+  attr_reader :id, :name, :rating, :image
 
 
   def initialize(data)
     @name = data["name"]
     @rating = data["original_game_rating"]
+    @image = data["image"]["medium_url"]
   end
 
   def self.search(query)
-    data = HTTParty.get(BASE_URL + "search/?api_key=#{ENV['GIANTBOMB_KEY']}&format=json&query='mario'&resources=game").parsed_response
-
+    data = HTTParty.get(BASE_URL + "search/?api_key=#{ENV['GIANTBOMB_KEY']}&format=json&query=#{query}&resources=game").parsed_response
     self.new(data["results"].sample)
   end
 
