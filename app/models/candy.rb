09dae469e5064bcd185dd5ed1fe975f80020c59e
Valerie <val.conklin@gmail.com>
2016-06-03 19:43:23 -0600

    Wrote a find_category method, not working quite yet but its intent is to find the correct category ID to add to the API search URL

--- app/models/candy.rb
+++ app/models/candy.rb
@@ -10,8 +10,28 @@ class Candy
     @name = data["name"]
   end
 
+  def self.find_category(category_name)
+    # returns the ID of the category with a name matching the query
+    data = HTTParty.get(BASE_URL + "").parsed_response
+    # iterate through the array of categories until the name matches
+    data["categories"].each do |category|
+      if category["name"] == category_name
+        return category["id"]
+      else
+        # in case the category_name actually matches a category WITHIN that category
+        # THIS WILL POTENTIALLY TAKE FOREVERRRR but it works I think
+        category["children"].each do |subcategory|
+            return subcategory["id"] if subcategory["name"] == category_name
+        end
+      end
+    end
+
+    return nil #if there is no such category at all
+  end
+
   def self.search(query)
-    data = HTTParty.get(BASE_URL + "search?apiKey=#{ENV['WALMART_KEY']}&query=#{query}").parsed_response
+    category_id = self.find_category("candy").to_i
+    data = HTTParty.get(BASE_URL + "search?apiKey=#{ENV['WALMART_KEY']}&query=#{query}&categoryId=#{category_id}").parsed_response
     self.new(data["items"][0])
   end
 
