771b8bb0dcf3ff96e0f431d20ea7505ccf564db8
Valerie <val.conklin@gmail.com>
2016-06-03 22:17:19 -0600

    Set up basic game search method, properly shows the name of a returned game in the view

--- app/controllers/combos_controller.rb
+++ app/controllers/combos_controller.rb
@@ -2,7 +2,9 @@ class CombosController < ApplicationController
 
   def index
     if params[:query]
+      # zip these together in combo_wrapper later
       @candy = Candy.search(params[:query])
+      @game = Game.search(params[:query])
     end
 
     # what to put in candy spot if there is no params
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,19 @@
-class Game < ActiveRecord::Base
+class Game
+
+  BASE_URL = "http://www.giantbomb.com/api/"
+  attr_reader :id, :name, :rating
+
+
+  def initialize(data)
+    @name = data["name"]
+    @rating = data["original_game_rating"]
+  end
+
+  def self.search(query)
+    data = HTTParty.get(BASE_URL + "search/?api_key=#{ENV['GIANTBOMB_KEY']}&format=json&query='mario'&resources=game").parsed_response
+
+    self.new(data["results"].sample)
+  end
+
+
 end
