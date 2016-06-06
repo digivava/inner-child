cb7ac5b0270a9b8870a9e289d0d6ebd48e9c6111
Valerie <val.conklin@gmail.com>
2016-06-05 21:05:21 -0700

    Adjusted Game.rb code so it no longer throws an error if no results are returned, and provided Pokemon Crystal as a default game in case of no E-rated results

--- app/models/game.rb
+++ app/models/game.rb
@@ -6,32 +6,40 @@ class Game
 
   def initialize(data)
     @name = data["name"]
-    # captures only ESRB ratings
-    @rating = data["original_game_rating"][0]["name"]
     @image = data["image"]["medium_url"]
   end
 
   def self.search(query)
     data = HTTParty.get(BASE_URL + "search/?api_key=#{ENV['GIANTBOMB_KEY']}&format=json&query=#{query}&resources=game").parsed_response
 
-
-    # a way to sort by original_game_rating until I can figure out how to make the
-    # URL do sort or filter properly...
-    data["results"].each do |game|
-      # games without ratings aren't considered kid-friendly.
-      # iterates until a rated-E game is found
-      if game["original_game_rating"] == nil || game["original_game_rating"][0]["name"] != "ESRB: E"
-        next
-      else
-        data = game
-        break data
-      end
-    end
-
-    if data["original_game_rating"][0]["name"] == "ESRB: E"
+    if data["number_of_total_results"] == 0
+      # to return SOMETHING nostalgic in case their query didn't match anything
+      data["name"] = "Pokemon Crystal Version"
+      data["image"] = "http://static.giantbomb.com/uploads/scale_medium/8/87790/2171280-box_pkmncv.png"
       self.new(data)
     else
-      raise "No child-friendly game found"
+      # a way to sort by original_game_rating until I can figure out how to make the
+      # URL do sort or filter properly...
+      data["results"].each do |game|
+        # games without ratings aren't considered kid-friendly.
+        # iterates until a rated-E game is found
+        # captures only ESRB ratings, not those from other countries, cuz I don't know how those work
+        if game["original_game_rating"] == nil || game["original_game_rating"][0]["name"] != "ESRB: E"
+          next
+        else
+          data = game
+          break data
+        end
+      end
+
+      if data["original_game_rating"][0]["name"] == "ESRB: E"
+        self.new(data)
+      else
+        # to return SOMETHING nostalgic in case an E-rated game wasn't found
+        data["name"] = "Pokemon Crystal Version"
+        data["image"] = "http://static.giantbomb.com/uploads/scale_medium/8/87790/2171280-box_pkmncv.png"
+        self.new(data)
+      end
     end
 
   end
